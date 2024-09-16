1fa01aaf7546c87509b403d31ef035ca876c581a
Mark Veskov <mark.veskov@abv.bg>
2024-09-16 14:38:01 +0300

    imported dotenv in dbConfig

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "your_database_name",
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
